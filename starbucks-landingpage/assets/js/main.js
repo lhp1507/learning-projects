16642dcf0ebb7a75518e34d372ff3d3512fe7fdf
Le Huu Phu <46106387+lhp1507@users.noreply.github.com>
2020-12-24 01:08:53 +0700

    Make responsive: 991px

--- starbucks-landingpage/assets/js/main.js
+++ starbucks-landingpage/assets/js/main.js
@@ -6,3 +6,13 @@ function changeCircleColor(color) {
   const circle = document.querySelector(".circle");
   circle.style.background = color;
 }
+
+function toggleMenu() {
+  var body = document.body;
+  var menuToggle = document.querySelector(".toggle");
+  var navigation = document.querySelector(".navigation");
+
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+  body.classList.toggle("noscroll");
+}
